269a3b02aa86cdb308ef52ff24a51c02455dd4b6
MoRahh <moodyaburahma@gmail.com>
2023-04-20 08:58:46 +0300

    Added the input and title Elements

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Videos Downloader")
 
+# Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10,pady=10)
+
+#Lnk Input
+link = customtkinter.CTkEntry(app, width=350, height=40)
+link.pack()
 
 # Run App
 app.mainloop()
+
